1ae980a9dfc4d06bf230e10f936c9990f7daae03
alexdavies440 <alexdavies440@gmail.com>
2024-11-09 20:52:51 -0600

    Almost done with Task 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,9 +1,16 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,6 +18,12 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Location required")
     @Size(min = 2, max = 100)
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+
+    // Consider whether this field should be final or not, if so get rid of setter
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
@@ -27,4 +40,11 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
