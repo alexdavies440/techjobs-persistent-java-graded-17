109e19a42daac0af80383e3baf36f5a61f1b0d33
alexdavies440 <alexdavies440@gmail.com>
2024-11-11 01:59:42 -0600

    Original query worked but test didn't like it, spent a good half hour trying to please the test but finally gave it what it wanted

--- queries.sql
+++ queries.sql
@@ -1,12 +1,26 @@
 --Part 1
 
---id INT,
---employer VARCHAR(255),
---name VARCHAR(255),
---skills VARCHAR(255)
+id INT,
+employer VARCHAR(255),
+name VARCHAR(255),
+skills VARCHAR(255)
 
 --Part 2
---SELECT name FROM employer WHERE location = "St. Louis City";
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 --Part 3
---DROP TABLE job;
+DROP TABLE job;
 --Part 4
+SELECT *
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
+
+--I feel like this should have passed...
+
+--SELECT DISTINCT skill.name
+--FROM skill
+--INNER JOIN job_skills ON (job_skills.skills_id = skill.id)
+--ORDER BY skill.name ASC;
